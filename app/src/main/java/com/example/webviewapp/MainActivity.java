5c2d60e8e8778723194b51a683a2d102b073ce09
a19jimsa <a19jimsa@student.his.se>
2021-04-01 10:40:00 +0200

    Last change...

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -18,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
+        myWebView.loadUrl("https://wwwlab.iit.his.se/a19jimsa/Mobilapplikationsdesignprojekt/");
     }
 
     public void showInternalWebPage(){
         // TODO: Add your code for showing internal web page here
+        myWebView.loadUrl("file:///android_asset/about.html");
     }
 
     @Override
@@ -85,13 +87,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
-            myWebView.loadUrl("https://wwwlab.iit.his.se/a19jimsa/Mobilapplikationsdesignprojekt/");
+            Log.d("==>","Will external internal web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
-            myWebView.loadUrl("file:///android_asset/about.html");
+            showInternalWebPage();
             return true;
         }
 
