738da6ca6c252d24a327fedd71eb4ed3443d0540
a19jimsa <a19jimsa@student.his.se>
2021-04-01 09:18:26 +0200

    menu terst

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
     }
